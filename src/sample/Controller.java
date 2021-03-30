7c6a3e91b909776d7dd9b425371a449244d02640
jyyblue <jyyblue1987@outlook.com>
2021-03-30 16:28:25 +0800

    [add] add GUI.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,16 @@
 package sample;
 
+import javafx.fxml.FXML;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+
 public class Controller {
+
+
+    public void initialize() {
+
+
+    }
 }
