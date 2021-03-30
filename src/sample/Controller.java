64c49fcb88bc2907ee2bd66100c5ec01f95245e8
jyyblue <jyyblue1987@outlook.com>
2021-03-30 16:54:24 +0800

    [add] add choice box

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,16 +1,57 @@
 package sample;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 
+import javax.xml.soap.Text;
+import java.awt.event.ActionEvent;
+
 public class Controller {
+    @FXML
+    TextField txtWeight = new TextField();
+
+    @FXML
+    TextField txtHeight = new TextField();
+
+    @FXML
+    ChoiceBox<String> cmbWeight = new ChoiceBox<>();
+
+    @FXML
+    ChoiceBox<String> cmbHeight = new ChoiceBox<>();
 
 
     public void initialize() {
+        cmbWeight.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                if(oldValue != newValue)
+                {
+                    String value = cmbWeight.getItems().get((Integer) newValue);
+                    txtWeight.setPromptText("Weight(" + value + ")");
+                }
+            }
+        });
 
+        cmbHeight.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                if(oldValue != newValue)
+                {
+                    String value = cmbHeight.getItems().get((Integer) newValue);
+                    txtHeight.setPromptText("Height(" + value + ")");
+                }
+            }
+        });
+    }
 
+    public void onClickCalculate(MouseEvent mouseEvent) {
     }
 }
