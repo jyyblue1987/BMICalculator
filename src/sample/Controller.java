b2ad17b4b87b41b78fbe6d9bf60f0e51da70dba3
jyyblue <jyyblue1987@outlook.com>
2021-03-30 17:13:40 +0800

    [add] add calculate

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -10,8 +10,8 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 
-import javax.xml.soap.Text;
 import java.awt.event.ActionEvent;
 
 public class Controller {
@@ -26,6 +26,14 @@ public class Controller {
 
     @FXML
     ChoiceBox<String> cmbHeight = new ChoiceBox<>();
+
+    @FXML
+    Text txtBMI = new Text();
+
+    @FXML
+    Text txtStatus = new Text();
+
+
 
 
     public void initialize() {
@@ -50,8 +58,60 @@ public class Controller {
                 }
             }
         });
+
+        txtWeight.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (!newValue.matches("\\d{0,7}([\\.]\\d{0,4})?")) {
+                    txtWeight.setText(oldValue);
+                }
+            }
+        });
+
+        txtHeight.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (!newValue.matches("\\d{0,7}([\\.]\\d{0,4})?")) {
+                    txtHeight.setText(oldValue);
+                }
+            }
+        });
+    }
+
+    protected double calculateBMI(double mass, double height) {
+        return mass / Math.pow(height/100.0, 2.0);
     }
 
     public void onClickCalculate(MouseEvent mouseEvent) {
+        double weight = Double.parseDouble(txtWeight.getText());
+        double height = Double.parseDouble(txtHeight.getText());
+        if( cmbWeight.getValue().equals("pounds") )
+        {
+            // convert pound to killo;
+            weight *= 0.45359237;
+        }
+
+        if( cmbHeight.getValue().equals("inches") )
+        {
+            height *= 2.54;
+        }
+
+        double bmi = calculateBMI(weight, height);
+        txtBMI.setText(String.format("%.2f", bmi));
+
+        String status = "";
+        if( bmi < 18.5 )
+            status = "Underweight";
+        else if( bmi < 24.9 )
+            status = "Normal Weight";
+        else if( bmi < 29.9)
+            status = "Overweight";
+        else
+            status = "Obesity";
+
+        txtStatus.setText(status);
+
     }
 }
