e9865da85890a6ea9d804b975c1a9440c04cf323
jyyblue <jyyblue1987@outlook.com>
2021-04-26 10:40:11 +0800

    [add] add diet/excersie

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -33,6 +34,11 @@ public class Controller {
     @FXML
     Text txtStatus = new Text();
 
+    @FXML
+    Text txtMessage = new Text();
+
+    @FXML
+    ImageView imgIcon;
 
 
     // when loading the page, this method is called automatically, so this initializes the event handler.
@@ -86,7 +92,8 @@ public class Controller {
         return mass / Math.pow(height/100.0, 2.0);
     }
 
-    public void onClickCalculate(MouseEvent mouseEvent) {
+    private double getBMIValue()
+    {
         double weight = Double.parseDouble(txtWeight.getText());
         double height = Double.parseDouble(txtHeight.getText());
         if(cmbWeight.getValue().equals("pounds") )
@@ -101,6 +108,12 @@ public class Controller {
         }
 
         double bmi = calculateBMI(weight, height);
+
+        return bmi;
+    }
+
+    public void onClickCalculate(MouseEvent mouseEvent) {
+        double bmi = getBMIValue();
         txtBMI.setText(String.format("%.2f", bmi));
 
         String status = "";
@@ -115,5 +128,56 @@ public class Controller {
 
         txtStatus.setText(status);
 
+    }
+
+    public void onClickDiet(MouseEvent mouseEvent) {
+        double bmi = getBMIValue();
+
+        String iconFile = "01.jpg";
+        if(bmi < 18.5) {
+            txtMessage.setText("Make sure to increase calorie intake.");
+            iconFile = "01.jpg";
+        }
+        else if(bmi < 24.9) {
+            txtMessage.setText("Keep doing what you're doing!");
+            iconFile = "02.jpg";
+        }
+        else if(bmi < 29.9) {
+            txtMessage.setText("Make sure to decrease your calorie intake! Avoid sugars as much as possible.");
+            iconFile = "03.jpg";
+        }
+        else if (bmi>30) {
+            txtMessage.setText("Decrease calorie intake and avoid all sugars.");
+            iconFile = "04.jpg";
+        }
+
+        Image image = new Image(getClass().getResource(iconFile).toExternalForm());
+        imgIcon.setImage(image);
+    }
+
+    public void onClickExcercise(MouseEvent mouseEvent) {
+        double bmi = getBMIValue();
+
+        String iconFile = "11.jpg";
+
+        if (bmi < 18.5) {
+            txtMessage.setText("Lift weights: Squats/deadlifts, bicep curls, triceps curls, bench press] Cardio: 30 min run.");
+            iconFile = "11.jpg";
+        }
+        else if(bmi < 24.9) {
+            txtMessage.setText("Lift weights: Squats/deadlifts, bicep curls, triceps curls, bench press] Cardio: 30 min run.");
+            iconFile = "12.jpg";
+        }
+        else if(bmi < 29.9) {
+            txtMessage.setText("Cardio: 30-60 min elliptical machine, 30-60 min walk, light running.");
+            iconFile = "13.jpg";
+        }
+        else if (bmi>30) {
+            txtMessage.setText("30-60 min in the pool, 30-60 min stationary bike.");
+            iconFile = "14.jpg";
+        }
+
+        Image image = new Image(getClass().getResource(iconFile).toExternalForm());
+        imgIcon.setImage(image);
     }
 }
